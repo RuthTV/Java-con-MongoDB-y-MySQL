4b9d313d1deeef2ca767c8f44163bde7a4ce728e
Telleria Viana <ruth.telleria.viana@accenture.com>
2023-02-13 13:19:27 +0100

    connection mysql

--- src/main/java/com/mySQL/mySQL/ConnectionMySQL.java
+++ src/main/java/com/mySQL/mySQL/ConnectionMySQL.java
@@ -8,12 +8,16 @@ public class ConnectionMySQL {
         String url = "jdbc:mysql://localhost:3306/midtermPoyect";
         String username = "ironhacker";
         String password = "Ir0nh4ck3r!";
+        String bases = "";
 
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from student");
+            while(resultSet.next()){
+                bases += resultSet.getString(1) + "\n";
+            }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
